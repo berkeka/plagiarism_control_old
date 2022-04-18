a7f10f0a9d71d8333f997bdcc5ea55f207a165ec
Berke Kalkan <berkekalkantr@gmail.com>
2022-04-18 22:19:27 +0300

    Change database configurations

---
+++ app/lib/database_url.rb
@@ -0,0 +1,24 @@
+module DatabaseUrl
+  module_function
+
+  def application
+    'plagiarism_control'
+  end
+
+  def url
+    host = ENV.fetch('DB_HOST', 'localhost')
+    "postgresql://#{application}:#{application}@#{host}/#{application}"
+  end
+
+  def development
+    "#{url}_development"
+  end
+
+  def production
+    "#{url}_production"
+  end
+
+  def test
+    "#{url}_test"
+  end
+end
