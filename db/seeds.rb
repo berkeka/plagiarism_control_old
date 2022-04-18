326036df9068f9f3a25e82327823b0d126ca7c1b
Berke Kalkan <berkekalkantr@gmail.com>
2022-04-18 22:46:27 +0300

    Fix rubocop offences

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ if Rails.env.development?
   # Seed first user
   [
     {
-      email:        'cezmi@stu.omu.edu.tr',
+      email: 'cezmi@stu.omu.edu.tr'
     }
   ].each do |args|
     User.create(**args, password: '123456', password_confirmation: '123456')
